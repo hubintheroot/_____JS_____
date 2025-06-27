253e76fa0803319433f5a22348ac06e7082c4aff
hubintheroot <hubintheroot@gmail.com>
2025-06-27 16:14:48 +0900

    feat/대소문자구분 없이 처리하는 기능 추가

--- day003/day_03.js
+++ day003/day_03.js
@@ -6,5 +6,5 @@
 
   //   02. words 중 l이 들어간 단어만 배열로 저장
   const words = ['hello', 'ghost', 'apple', 'grape', 'lemon'];
-  console.log(words.filter((w) => w.includes('l')));
+  console.log(words.filter((w) => w.toLowerCase().includes('l')));
 })();
