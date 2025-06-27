e5e067c3962e241aed1a1eeb72f8a3bd52ced249
hubintheroot <hubintheroot@gmail.com>
2025-06-27 16:02:44 +0900

    feat/과제파일 추가

---
+++ day003/day_03.js
@@ -0,0 +1,10 @@
+(() => {
+  // 01. heights에서 ghost보다 큰 사람 몇명인지 출력
+  const heights = [150, 169, 185, 170];
+  const ghost = 167;
+  console.log(heights.filter((t) => t > ghost).length);
+
+  //   02. words 중 l이 들어간 단어만 배열로 저장
+  const words = ['hello', 'ghost', 'apple', 'grape', 'lemon'];
+  console.log(words.filter((w) => w.includes('l')));
+})();
